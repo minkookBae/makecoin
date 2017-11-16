3a3f336814a00aa3cfb311eeea12d2150080fe90
MK <alsrnr1103@naver.com>
2017-11-16 21:00:28 +0900

    Add files via upload

--- makecoin/main/views.py
+++ makecoin/main/views.py
@@ -1,7 +1,6 @@
-
 from django.http import JsonResponse
-# Create your views here.
-from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def index(request):
 	return render(request, 'main.html',{})
@@ -11,3 +10,19 @@ def keyboard(request):
 		'type' : 'buttons',
 		'buttons' : ['비트코인','이더리움']
 		})
+
+
+def message(request):
+        message = ((request.body).decode('utf-8'))
+        return_json_str = json.loads(message)
+        return_str = return_json_str['content']
+ 
+        return JsonResponse({
+                'message': {
+                        'text': "you type "+return_str+"!"
+                },
+                'keyboard': {
+                        'type': 'buttons',
+                        'buttons': ['1','2']
+                }
+        })
